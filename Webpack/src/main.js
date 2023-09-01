4a55bff0d5158053b0b27be1f345b574ba25d3c9
Alisson Alves <alisson.asilva3@gmail.com>
2023-09-01 10:30:45 -0300

    (fix) Webpack CSS config

--- Webpack/src/main.js
+++ Webpack/src/main.js
@@ -1,6 +1,7 @@
 // const Person = require('./person')
 
-import Person from "./person";
+import Person from './person';
+import './assets/css/style.css';
 
 const attendant = new Person
 console.log(attendant.greet())
--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -6,5 +6,16 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: __dirname + '/public'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',         // Adiciona CSS a DOM injetando a tag <style>
+                    'css-loader'        // Interpreta @import, url()...
+                ]
+            }
+        ]
     }
 }
